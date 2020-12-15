6524a1f5513fb936401c2c65c4b69b81bfd9d1b3
Madhon <madhon@madhon.co.uk>
2020-12-15 11:32:57 +0000

    port to .Net 50

--- src/CoreDep/Program.cs
+++ src/CoreDep/Program.cs
@@ -12,6 +12,7 @@ namespace CoreDep
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSystemd()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
