e208cf72a315e619b5b8bf70d97deb9d61142ae3
Madhon <madhon@madhon.co.uk>
2021-02-22 14:40:52 +0000

    feat: commitizen pre-commit

--- src/CoreDep/Controllers/HomeController.cs
+++ src/CoreDep/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿namespace CoreDep.Controllers
 {
     using System.Diagnostics;
+    using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.Logging;
     using Models;
@@ -14,16 +15,22 @@
             _logger = logger;
         }
 
+        [HttpGet()]
+        [AllowAnonymous()]
         public IActionResult Index()
         {
             return View();
         }
 
+        [HttpGet()]
+        [AllowAnonymous()]
         public IActionResult Privacy()
         {
             return View();
         }
 
+        [HttpGet()]
+        [AllowAnonymous()]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
